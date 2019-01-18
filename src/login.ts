8873c9c0340737a1ec37cd7e991a3c2da8d61815
1412369@student.hcmus.edu.vn <nhanlt@blockchainlabs.asia>
2019-01-18 15:04:50 +0700

    update sdk

--- src/login.ts
+++ src/login.ts
@@ -1,9 +1,9 @@
 import {AuthenURL, ButtonId} from './config';
 export const loginCallback = (callback: (success: any, error: any) => void) => {
-  window.addEventListener('message', (e: any) => {
+  window.addEventListener('message', (e: MessageEvent) => {
     try {
       if (e.data) {
-        callback(JSON.parse(e.data || {}), null);
+        callback(e, null);
       } else {
         callback(null, 'message error');
       }
@@ -20,23 +20,29 @@ export const initAuthenScript = () => {
       if (!loginBtn) {
         throw `Quanta login button notfound \n Please add id=${ButtonId} into login tag`;
       }
-      if (loginBtn.dataset.loginType === 'dialog') {
-        const width = 780;
-        const height = 700;
-        const left = screen.width / 2 - width / 2;
-        const top = screen.height / 2 - height / 2;
-        const features = `width=${width}, height=${height}, top=${top}, left=${left}`;
-        loginBtn.addEventListener('click', () => {
+      loginBtn.addEventListener('click', () => {
+        if (
+          loginBtn.dataset.loginType &&
+          loginBtn.dataset.loginType === 'dialog'
+        ) {
+          const width = 780;
+          const height = 700;
+          const left = screen.width / 2 - width / 2;
+          const top = screen.height / 2 - height / 2;
+          const features = `width=${width}, height=${height}, top=${top}, left=${left}`;
           window.open(
-            `${AuthenURL}?client_id=${loginBtn.dataset.clientId}`,
+            `${'./dialog.html'}?client_id=${loginBtn.dataset.clientId}`,
             'Login to Quanta Game Platform ',
             features,
           );
-        });
-      }
-      window.addEventListener('message', (e: any) => {
-        console.log('Receiver event: ', e.data);
-        document.location.href = `${loginBtn.dataset.redirect}?code=123123123`;
+        } else {
+          if (!loginBtn.dataset.redirect) {
+            throw `if data-login-type='dialog' is not set, data-redirect must be provied`;
+          }
+          window.location.href = `${AuthenURL}?callback=${
+            loginBtn.dataset.redirect
+          }`;
+        }
       });
     } catch (e) {
       console.error(e);
